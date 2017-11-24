d55ae7e1c245cec685695a27905bd1d24d8cf653
Jarosław Wójcik <j.wojcik@interia.eu>
2017-11-24 13:27:50 +0100

    Add js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Triangle Field
+var h= prompt("Enter height h");
+var a= prompt("Enter base a");
+var triangleArea= (a*h)/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+document.write('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
